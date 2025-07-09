96070d18a5d11c34899dfba645c1f841b3da29a3
cpp_project1 <kdpadaliya216@gmail.com>
2025-07-09 13:23:11 +0530

    Fix

--- lib/features/employees/presentation/pages/employees_screen.dart
+++ lib/features/employees/presentation/pages/employees_screen.dart
@@ -160,55 +160,39 @@ class _EmployeesScreenState extends State<EmployeesScreen> {
 
             /// Employee List
             Expanded(
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  final screenWidth = constraints.maxWidth;
-                  final spacing = 12.0 * Responsive.getResponsive(context);
-                  final itemCount = screenWidth > 1200
-                      ? 12
-                      : screenWidth > 900
-                      ? 9
-                      : screenWidth > 600
-                      ? 6
-                      : 3;
-                  final itemWidth =
-                      (screenWidth - (spacing * (itemCount - 1))) / itemCount;
-                  final itemHeight = screenWidth > 600 ? 180.0 : 150.0;
-                  final childAspectRatio = itemWidth / itemHeight;
-
-                  return GridView.builder(
-                    padding: const EdgeInsets.only(
-                      top: 6,
-                      bottom: 5,
-                      left: 5,
-                      right: 5,
-                    ),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: itemCount,
-                      crossAxisSpacing: spacing,
-                      mainAxisSpacing: spacing,
-                      childAspectRatio: childAspectRatio,
-                    ),
-                    itemCount: employees.length,
-                    itemBuilder: (context, index) {
-                      final emp = employees[index];
-                      return EmployeeSelectionCard(
-                        name: emp['name']!,
-                        department: emp['role']!,
-                        image: NetworkImage(emp['image']!),
-                        showDelete: true,
-                        // selectedColor: Colors.red,
-                        isSelected: selectedEmployeeIndexes.contains(index),
-                        onSelected: (value) {
-                          setState(() {
-                            if (selectedEmployeeIndexes.contains(index)) {
-                              selectedEmployeeIndexes.remove(index);
-                            } else {
-                              selectedEmployeeIndexes.add(index);
-                            }
-                          });
-                        },
-                      );
+              child: GridView.builder(
+                padding: const EdgeInsets.only(
+                  top: 6,
+                  bottom: 5,
+                  left: 5,
+                  right: 5,
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: Responsive.getGridConfig(context).itemCount,
+                  mainAxisSpacing: Responsive.getGridConfig(context).spacing,
+                  crossAxisSpacing: Responsive.getGridConfig(context).spacing,
+                  childAspectRatio: Responsive.getGridConfig(
+                    context,
+                  ).childAspectRatio,
+                ),
+                itemCount: employees.length,
+                itemBuilder: (context, index) {
+                  final emp = employees[index];
+                  return EmployeeSelectionCard(
+                    name: emp['name']!,
+                    department: emp['role']!,
+                    image: NetworkImage(emp['image']!),
+                    showDelete: true,
+                    // selectedColor: Colors.red,
+                    isSelected: selectedEmployeeIndexes.contains(index),
+                    onSelected: (value) {
+                      setState(() {
+                        if (selectedEmployeeIndexes.contains(index)) {
+                          selectedEmployeeIndexes.remove(index);
+                        } else {
+                          selectedEmployeeIndexes.add(index);
+                        }
+                      });
                     },
                   );
                 },
