118bd7ab4c2eaf19793a8e88b4c93bce28bfe07c
aasif-shaykh <aasif.shaikh@chplgroup.org>
2025-07-09 10:57:01 +0530

    Code Updated

--- lib/di/injector.dart
+++ lib/di/injector.dart
@@ -5,8 +5,8 @@ import 'package:myco_flutter/di/modules/network_module.dart';
 import 'package:myco_flutter/features/admin_view/presentation/di/admin_view_di.dart';
 import 'package:myco_flutter/features/company_selector/presentation/di/company_select_di.dart';
 import 'package:myco_flutter/features/language_selector/presentation/di/language_di.dart';
-import 'package:myco_flutter/features/splash/presentation/di/splash_di.dart';
 import 'package:myco_flutter/features/my_visit/presentation/di/face_detection_di.dart';
+import 'package:myco_flutter/features/splash/presentation/di/splash_di.dart';
 
 final sl = GetIt.instance;
 
