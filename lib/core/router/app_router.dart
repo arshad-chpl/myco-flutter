2e175da4b8bfe98d5ef94a97bf63e8f6229ef79a
ayush-padaliya-chpl <167543532+ayush-padaliya-chpl@users.noreply.github.com>
2025-07-11 18:10:09 +0530

    --stable-- app router changes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -61,7 +61,7 @@ final _rootNavigatorKey = GlobalKey<NavigatorState>();
 class AppRouter {
   final GoRouter router = GoRouter(
     navigatorKey: _rootNavigatorKey,
-    initialLocation: RoutePaths.assetsHome,
+    initialLocation: RoutePaths.getStarted,
     // initialLocation: RoutePaths.dashboard,
     observers: [
       // FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
