124e9d14a615906383b232215493dfe5e8f13d8c
ajmeriajaj <ajmeriajaj9@gmail.com>
2025-07-16 09:57:16 +0530

    ::NEW:: create work_allocation module

---
+++ lib/features/work_allocation/presentation/pages/work_allocation_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WorkAllocationPage extends StatefulWidget {
+  const WorkAllocationPage({super.key});
+
+  @override
+  State<WorkAllocationPage> createState() => _WorkAllocationPageState();
+}
+
+class _WorkAllocationPageState extends State<WorkAllocationPage> {
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
