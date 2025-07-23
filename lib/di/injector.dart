662f6f6f83ebb2cfe527b35ba281eadb59cea792
ajmeriajaj <ajmeriajaj9@gmail.com>
2025-07-23 16:29:48 +0530

    Appointment : Completed appointmentRequest and call api

--- lib/di/injector.dart
+++ lib/di/injector.dart
@@ -13,7 +13,6 @@ import 'package:myco_flutter/features/language_selector/presentation/di/language
 import 'package:myco_flutter/features/leave/presentation/di/leave_di.dart';
 import 'package:myco_flutter/features/my_visit/presentation/di/face_detection_di.dart';
 import 'package:myco_flutter/features/my_visit/presentation/di/visit_with_di.dart';
-import 'package:myco_flutter/features/my_visit/presentation/di/face_detection_di.dart';
 import 'package:myco_flutter/features/payslip/presentation/di/payslip_di.dart';
 import 'package:myco_flutter/features/splash/presentation/di/splash_di.dart';
 import 'package:myco_flutter/features/work_allocation/presentation/di/work_allocation_di.dart';
