d7acc1e237107df19fab40731410079e9932e828
ajmeriajaj <ajmeriajaj9@gmail.com>
2025-07-25 13:24:21 +0530

    Appointment : Completed MyAppointment api and add shimmer for both tabPages

--- lib/features/appointments/presentation/pages/reject_request_bottom_sheet.dart
+++ lib/features/appointments/presentation/pages/reject_request_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get_it/get_it.dart';
 import 'package:myco_flutter/constants/app_assets.dart';
 import 'package:myco_flutter/constants/constants.dart';
